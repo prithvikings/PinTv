1e1fb5dd77e1a298c0b904407b598df91e2cdd6c
prithvikings <prithvi07raj07@gmail.com>
2025-02-11 23:56:38 +0530

    Add loading animations, user context, and account management features

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { UserData } from "./context/UserContext";
+import { Loading } from "./components/Loading";
+import Navbar from "./components/Navbar";
+import PinPage from "./pages/PinPage";
+import Create from "./pages/Create";
+import Account from "./pages/Account";
+import UserProfile from "./pages/UserProfile";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { loading, isAuth, user } = UserData();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          {isAuth && <Navbar user={user} />}
+          <Routes>
+            <Route path="/" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/account"
+              element={isAuth ? <Account user={user} /> : <Login />}
+            />
+            <Route
+              path="/user/:id"
+              element={isAuth ? <UserProfile user={user} /> : <Login />}
+            />
+            <Route path="/create" element={isAuth ? <Create /> : <Login />} />
+            <Route
+              path="/pin/:id"
+              element={isAuth ? <PinPage user={user} /> : <Login />}
+            />
+            <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/register"
+              element={isAuth ? <Home /> : <Register />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserProvider } from "./context/UserContext.jsx";
+import { PinProvider } from "./context/PinContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UserProvider>
+      <PinProvider>
+        <App />
+      </PinProvider>
+    </UserProvider>
+  </React.StrictMode>
+);
